16c3bbddaa7fd4d0f077d901fc43015b81bf222f
Daniel Kohkemper <dkohkemper@gmail.com>
2019-11-09 23:22:16 -0600

    Uploading algorithm and test files for LMS calculations

---
+++ tarea_02/src/lms_4_e_nlms_pow_norm.m
@@ -0,0 +1,37 @@
+% LMS_4_E_NLMS_POW_NORM
+%   [K, W_VEC] = lms_4_e_nlms_pow_norm(D_VAR, U_VEC, W_INIT, TOL, ITER_MAX) solves problem
+%   min_{w}E[|d-uw|^{2}]
+%   
+%   d_var:    random variable
+%   u_vec:    random row vector size 1xn
+%   w_init:   initial w vector w^{(-1)}
+%   tol:      tolerance
+%   iter_max: max number of iterations
+%
+%   idx_k:    number of iterations achieved
+%   w_vector: constant column vector size nx1
+%   error:    approximation error
+%   min_val:  approximated minimum value
+%
+% *************************************************************************
+function [idx_k, w_vector, error, min_val] = lms_4_e_nlms_pow_norm(d_var, u_vec, w_init_vec, tol, iter_max)
+    
+    % TODO: Delete, put here just to avoid messages 
+    d_var      = d_var;
+    u_vec      = u_vec;
+    w_init_vec = w_init_vec;
+    tol        = tol;
+    iter_max   = iter_max;
+    
+    % TIP: Use (||w^{k} - w^{k-1}||_2 < tol) or (idx_k == iter_max) to stop algorithm
+    
+    % TODO:
+    % Calculate error: e_{k} = ||w^{k} - w^{k-1}||_2
+    % Calculate min value
+    
+    % Output variables
+    idx_k    = 0;
+    w_vector = [];
+    error    = 0;
+    min_val  = 0;
+end
---
+++ tarea_02/test/lms_test_2.m
@@ -0,0 +1,31 @@
+% *************************************************************************
+% TEST 1: 
+% *************************************************************************
+clc;
+clear;
+close('all');
+addpath 'src';
+
+fprintf('Simulation start\n');
+
+% Set constants
+d_var      = [];
+u_vec      = [];
+w_init_vec = [];
+tol        = 0;
+iter_max   = 0;
+
+[idx_k, w_vector, error, min_val] = lms_2_var_step(d_var, u_vec, w_init_vec, tol, iter_max);
+
+% TODO: Uncomment when function works
+% figure(1);
+% stem(idx_k, error);
+% xlabel("Iterarations");
+% ylabel("Error e_k");
+% title("Test 1:Iterations vs general error e_k")
+% 
+% figure(2);
+% stem(idx_k, min_val);
+% xlabel("Iterarations");
+% ylabel("Min value");
+% title("Test 1:Iterations vs minimun value")
