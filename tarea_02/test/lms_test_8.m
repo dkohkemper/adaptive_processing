d3eaa403ff18d1057f62928a95da8297a518e40f
Daniel Kohkemper <dkohkemper@gmail.com>
2019-11-16 12:36:18 -0600

    Completed LMS 8 algorithm

--- tarea_02/test/lms_test_8.m
+++ tarea_02/test/lms_test_8.m
@@ -1,5 +1,5 @@
 % *************************************************************************
-% TEST 1: 
+% TEST 8:
 % *************************************************************************
 clc;
 clear;
@@ -9,25 +9,26 @@ addpath 'src';
 fprintf('Simulation start\n');
 
 % Set constants
-d_var      = csvread("../input/d.text");
-u_vec      = csvread("../input/U.text");
-w_init_vec = [];
-tol        = 0;
-iter_max   = 0;
+d_var      = csvread("input/d.text");
+u_vec      = csvread("input/U.text");
+w_init_vec = zeros(size(u_vec, 2),1);
+tol        = 10^-6;
+iter_max   = size(u_vec, 1);
+mu         = 0.0001;
+delta      = 0.5;
 
-[idx_k, w_vector, error, min_val] = lms_8_lmmn(d_var, u_vec, w_init_vec, tol, iter_max);
+[idx_k, w_vector, error_vec, min_val_vec] = lms_8_lmmn(d_var, u_vec, w_init_vec, mu, delta, tol, iter_max);
 
-% TODO: Uncomment when function works
-% figure(1);
-% stem(idx_k, error);
-% xlabel("Iterarations");
-% ylabel("Error e_k");
-% title("Test 1:Iterations vs general error e_k")
-% 
-% figure(2);
-% stem(idx_k, min_val);
-% xlabel("Iterarations");
-% ylabel("Min value");
-% title("Test 1:Iterations vs minimun value")
+figure(1);
+plot((1 : idx_k), error_vec);
+xlabel("Iterarations");
+ylabel("Error e_k");
+title("Test 8: General error e_k vs Iterations")
+
+figure(2);
+plot((1 : idx_k), min_val_vec);
+xlabel("Iterarations");
+ylabel("Min value");
+title("Test 8: Minimun value vs Iterations")
 
 fprintf('Simulation end\n');
