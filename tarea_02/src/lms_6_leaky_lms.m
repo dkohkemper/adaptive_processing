1fc5841baf4ec84950c93fd1cf6a73906e5e5da7
Daniel Kohkemper <dkohkemper@gmail.com>
2019-11-16 12:47:30 -0600

    Completed LMS 6 algorithm

--- tarea_02/src/lms_6_leaky_lms.m
+++ tarea_02/src/lms_6_leaky_lms.m
@@ -1,45 +1,56 @@
 % LMS_6_LEAKY_LMS
-%   [K, W_VEC] = lms_6_leaky_lms(D_VAR, U_VEC, W_INIT, TOL, ITER_MAX) solves problem
+%   [K, W_VEC] = lms_6_leaky_lms(D_VAR, U_VEC, W_INIT, MU, ALPHA, TOL, ITER_MAX) solves problem
 %   min_{w}E[|d-uw|^{2}]
 %   
 %   d_var:    random variable
 %   u_vec:    random row vector size 1xn
 %   w_init:   initial w vector w^{(-1)}
+%   mu:       mu constant
+%   alpha:    alpha constant
 %   tol:      tolerance
 %   iter_max: max number of iterations
 %
-%   idx_k:    number of iterations achieved
-%   w_vector: constant column vector size nx1
-%   error:    approximation error
-%   min_val:  approximated minimum value
+%   idx_k:        number of iterations achieved
+%   w_vector:     constant column vector size nx1
+%   error_vec:    approximation error
+%   min_val_vec:  approximated minimum value
 %
 % *************************************************************************
-function [idx_k, w_vector, error, min_val] = lms_6_leaky_lms(d_var, u_vec, w_init_vec, tol, iter_max)
-    
-    % TODO: Delete, put here just to avoid messages 
-    d_var      = d_var;
-    u_vec      = u_vec;
-    w_init_vec = w_init_vec;
-    tol        = tol;
-    iter_max   = iter_max;
-    
-    % TEST: Inspect size and data type of random variable samples 'd'
-    s = length(d_var);
-    fprintf('TEST: random variable d_var: size (1x%d) and type info %s\n', s, typeinfo(d_var));
+function [idx_k, w_vector, error_vec, min_val_vec] = lms_6_leaky_lms(d_var, u_vec, w_init, mu, alpha, tol, iter_max)
+    % Declare output vectors
+    error_vec   = [];
+    min_val_vec = [];
+    % Init w_vector
+    w_vector = w_init;
 
-    % TEST: Inspect size and data type of random vector samples 'u' (matrix 'U')
-    [s, n] = size(u_vec);
-    fprintf('TEST: random vector u_vec: size (%dx%d) and type info %s\n', s, n, typeinfo(u_vec));
+    for idx_k = 1 : length(d_var)
+        % Calculate estimation error
+        e_i = (d_var(idx_k) - u_vec(idx_k, :) * w_vector);
+        % Calculate w vector
+        w_next = (1 - mu * alpha) * w_vector + mu * ctranspose(u_vec(idx_k, :)) * e_i;
+        % Calculate error e_{k} = ||w^{k} - w^{k-1}||_2
+        error_vec = [error_vec, norm(w_next - w_vector)];
+        % Calculate min_value
+        R_dk_dk = d_var(idx_k) * ctranspose(d_var(idx_k));
+        R_dk_uk = d_var(idx_k) * ctranspose(u_vec(idx_k, :));
+        R_uk_uk = ctranspose(u_vec(idx_k, :)) * u_vec(idx_k, :);
+        min_val = R_dk_dk - ...
+                  ctranspose(R_dk_uk)  * w_vector - ...
+                  ctranspose(w_vector) * R_dk_uk + ...
+                  ctranspose(w_vector) * R_uk_uk * w_vector;
+        % Append min_val to output vector
+        min_val_vec = [min_val_vec, min_val];
 
-    % TIP: Use (||w^{k} - w^{k-1}||_2 < tol) or (idx_k == iter_max) to stop algorithm
+        % Update w vector value
+        w_vector = w_next;
 
-    % TODO:
-    % Calculate error: e_{k} = ||w^{k} - w^{k-1}||_2
-    % Calculate min value
-    
-    % Output variables
-    idx_k    = 0;
-    w_vector = [];
-    error    = 0;
-    min_val  = 0;
+        % Break if tolerance is reached (||w^{k} - w^{k-1}||_2 < tol)
+        if(error_vec(end) < tol)
+            break;
+        end
+        % Break loop if max iteration number is reached
+        if(idx_k == iter_max)
+            break
+        end
+    end
 end
