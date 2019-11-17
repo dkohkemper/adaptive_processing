a25cd4ce8268770a18902e558a9eba5cad49dcaa
fabisqc0207 <fabisqc0207@gmail.com>
2019-11-16 20:45:50 -0600

    Add implementation and its test bench for Gauss-Newton algorithm

--- tarea_02/src/lms_10_gauss_newton.m
+++ tarea_02/src/lms_10_gauss_newton.m
@@ -5,41 +5,67 @@
 %   d_var:    random variable
 %   u_vec:    random row vector size 1xn
 %   w_init:   initial w vector w^{(-1)}
+%   alpha:    alpha small factor (0 < alpha < 0.1)
+%   epsilon:  a-priori error
+%   lambda:   forgetting factor
+%   mu:       mu constant
 %   tol:      tolerance
 %   iter_max: max number of iterations
 %
 %   idx_k:    number of iterations achieved
 %   w_vector: constant column vector size nx1
-%   error:    approximation error
-%   min_val:  approximated minimum value
+%   error_vec:    approximation error
+%   min_val_vec:  approximated minimum value
 %
 % *************************************************************************
-function [idx_k, w_vector, error, min_val] = lms_10_gauss_newton(d_var, u_vec, w_init_vec, tol, iter_max)
-    
-    % TODO: Delete, put here just to avoid messages 
-    d_var      = d_var;
-    u_vec      = u_vec;
-    w_init_vec = w_init_vec;
-    tol        = tol;
-    iter_max   = iter_max;
-    
-    % TEST: Inspect size and data type of random variable samples 'd'
-    s = length(d_var);
-    fprintf('TEST: random variable d_var: size (1x%d) and type info %s\n', s, typeinfo(d_var));
-
-    % TEST: Inspect size and data type of random vector samples 'u' (matrix 'U')
+function [idx_k, w_vector, error_vec, min_val_vec] = lms_10_gauss_newton(d_var, u_vec, w_init, alpha, epsilon, lambda, mu, tol, iter_max)
+    % Init constants
     [s, n] = size(u_vec);
-    fprintf('TEST: random vector u_vec: size (%dx%d) and type info %s\n', s, n, typeinfo(u_vec));
+    lambda_1 = lambda^-1;
+    P_1 = (epsilon^-1)*eye(n);
+    % Init w_vector
+    w_vector = w_init;
+    % Init P matrix
+    P_matrix = P_1;
+    % Declare output vectors
+    error_vec   = [];
+    min_val_vec = [];
+    
+    for idx_k = 1 : s
+        % Calculate estimation (a-posteriori) error
+        e_i = (d_var(idx_k) - u_vec(idx_k, :) * w_vector);
+        % Calculate P matrix
+        P_sub  = (lambda_1 * P_matrix * ctranspose(u_vec(idx_k, :)) * u_vec(idx_k, :) * P_matrix) / ...
+                 ((1 - alpha)/alpha + lambda_1 * u_vec(idx_k, :) * P_matrix * ctranspose(u_vec(idx_k, :)));
+        P_next = (lambda_1/1 - alpha)*(P_matrix - P_sub);
+        % Calculate w vector
+        w_next = w_vector + mu * P_next * ctranspose(u_vec(idx_k, :)) * e_i;
+        % Calculate error e_{k} = ||w^{k} - w^{k-1}||_2
+        error_val = norm(w_next - w_vector);
+        error_vec = [error_vec, error_val];
+        % Calculate min_value
+        R_dk_dk = d_var(idx_k) * ctranspose(d_var(idx_k));
+        R_dk_uk = d_var(idx_k) * ctranspose(u_vec(idx_k, :));
+        R_uk_uk = ctranspose(u_vec(idx_k, :)) * u_vec(idx_k, :);
+        min_val = R_dk_dk - ...
+                  ctranspose(R_dk_uk)  * w_vector - ...
+                  ctranspose(w_vector) * R_dk_uk + ...
+                  ctranspose(w_vector) * R_uk_uk * w_vector;
+        % Append min_val to output vector
+        min_val_vec = [min_val_vec, min_val];
 
-    % TIP: Use (||w^{k} - w^{k-1}||_2 < tol) or (idx_k == iter_max) to stop algorithm
-    
-    % TODO:
-    % Calculate error: e_{k} = ||w^{k} - w^{k-1}||_2
-    % Calculate min value
-    
-    % Output variables
-    idx_k    = 0;
-    w_vector = [];
-    error    = 0;
-    min_val  = 0;
+        % Update P matrix
+        P_matrix = P_next;
+        % Update w vector
+        w_vector = w_next;
+
+        % Break if tolerance is reached (||w^{k} - w^{k-1}||_2 < tol)
+        if(error_vec(end) < tol)
+            break;
+        end
+        % Break loop if max iteration number is reached
+        if(idx_k == iter_max)
+            break
+        end
+    end
 end
