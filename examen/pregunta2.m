c7e26b115cd6b082562391e897cd3fb16c6b94f2
Daniel Kohkemper <dkohkemper@gmail.com>
2019-12-10 00:25:31 -0600

    Adding less buggier code

--- examen/pregunta2.m
+++ examen/pregunta2.m
@@ -27,7 +27,7 @@ fclose("all");
 
 ro       = 0.4;
 sigma    = 2;
-dim      = 4;
+dim      = 30;
 tol      = 10^-4;
 max_iter = 1000;
 k1       = 15;
@@ -51,12 +51,12 @@ A1_mat = full(gallery('tridiag', dim, randn, randn, randn));
 A2_mat = full(gallery('tridiag', dim, randn, randn, randn));
 
 % Generate random C_i and B_i initial matrixes
-B1_mat = rand(dim, dim);
-B2_mat = rand(dim, dim);
-C1_mat = rand(dim, dim);
-C2_mat = rand(dim, dim);
+B1_mat = rand(dim, k1);
+B2_mat = rand(dim, k2);
+C1_mat = rand(k1, dim);
+C2_mat = rand(k2, dim);
 
-% Iteraciones
+% Declare vectors
 error_vec =[];
 min_vec   =[];
 
@@ -69,40 +69,24 @@ R_sx1  = R_ss   * A1_mat';
 R_sx2  = R_ss   * A2_mat';
 R_x2s  = A2_mat * R_ss;
 
-% Calculate aditional covariance matrixes
-% mat1 = C2_mat' * (C2_mat * R_x2x2 * C2_mat')^(-1) * C2_mat;
-% R_ee = R_x1x1 - R_x1x2 * mat1 * R_x2x1;
-% R_vv = R_ss   - R_sx2  * mat1 * R_x2s;
-% R_ve = R_sx1  - R_sx2  * mat1 * R_x2x1;
-% % R_ee = R_x1x1 - R_x1x2 * C2_mat' * (C2_mat * R_x2x2 * C2_mat')^(-1) * C2_mat * R_x2x1;
-% % R_vv = R_ss   - R_sx2  * C2_mat' * (C2_mat * R_x2x2 * C2_mat')^(-1) * C2_mat * R_x2s;
-% % R_ve = R_sx1  - R_sx2  * C2_mat' * (C2_mat * R_x2x2 * C2_mat')^(-1) * C2_mat * R_x2x1;
-
-% temp_mat = R_ve * R_ee^(-1) * R_ve';
-% [u_vec, lambda] = eig(temp_mat);
-
-% Calculate initial values of B1, B2, C1, C2
-% C1_mat = u_vec' * R_ve * R_ee^(-1);
-% C2_mat = u_vec' * R_ve * R_ee^(-1);
-% B1_mat = u_vec';
-% B2_mat = (R_sx2 - B1_mat * C1_mat * R_x1x2) * mat1;
-
+% Loop
 for k = 1 : max_iter
 
     % Calculate aditional covariance matrixes
-    mat1 = C2_mat' * (C2_mat * R_x2x2 * C2_mat')^(-1) * C2_mat;
-    R_ee = R_x1x1 - R_x1x2 * mat1 * R_x2x1;
-    R_vv = R_ss   - R_sx2  * mat1 * R_x2s;
-    R_ve = R_sx1  - R_sx2  * mat1 * R_x2x1;
+    mat1 = C2_mat' * (C2_mat * R_x2x2 * C2_mat')^(-1);
+    mat2 = mat1 * C2_mat;
+    R_ee = R_x1x1 - R_x1x2 * mat2 * R_x2x1;
+    R_vv = R_ss   - R_sx2  * mat2 * R_x2s;
+    R_ve = R_sx1  - R_sx2  * mat2 * R_x2x1;
 
     % Obtain eigenvalues matrix
     temp_mat = R_ve * R_ee^(-1) * R_ve';
     [u_vec, lambda] = eig(temp_mat);
 
     % Calculate initial values of B1, B2, C1, C2
-    C1_mat_n = u_vec' * R_ve * R_ee^(-1);
-    C2_mat_n = u_vec' * R_ve * R_ee^(-1);
-    B1_mat_n = u_vec';
+    C1_mat_n = u_vec(: , 1:k1)' * R_ve * R_ee^(-1);
+    C2_mat_n = u_vec(: , 1:k1)' * R_ve * R_ee^(-1);
+    B1_mat_n = u_vec(: , 1:k1);
     B2_mat_n = (R_sx2 - B1_mat * C1_mat * R_x1x2) * mat1;
 
     % Calculate norms
@@ -110,9 +94,10 @@ for k = 1 : max_iter
     norm_B2 = frob_norm((B2_mat_n - B2_mat));
     norm_C1 = frob_norm((C1_mat_n - C1_mat));
     norm_C2 = frob_norm((C2_mat_n - C2_mat));
+
     % Calculate total error
     tot_error = 0.25 * (norm_B1 + norm_B2 + norm_C1 + norm_C2);
-
+    % Generate error vector
     error_vec = [error_vec, tot_error];
 
     % Update matrixes
@@ -130,13 +115,20 @@ for k = 1 : max_iter
     R_sx2  = R_ss   * A2_mat';
     R_x2s  = A2_mat * R_ss;
 
-
     if tot_error < tol
         break
     end
 end
 
+tot_error
+
 % Generate the following graphs
 %   error vs iterations
 %   min vs iterations
 %   min vs k
+
+figure(1);
+plot(1:k, error_vec);
+xlabel("Dimension m");
+ylabel("General error e_k");
+title("Test 1: Dimension m vs general error e_k")
