8fd87a2a9245cbce4363fb0286de5368fd4376d3
Daniel Kohkemper <dkohkemper@gmail.com>
2019-12-09 23:25:06 -0600

    Adding loop code, still buggy

--- examen/pregunta2.m
+++ examen/pregunta2.m
@@ -30,6 +30,8 @@ sigma    = 2;
 dim      = 4;
 tol      = 10^-4;
 max_iter = 1000;
+k1       = 15;
+k2       = 15;
 
 % Generate s matrix
 s_mat = rand(dim, dim);
@@ -44,7 +46,7 @@ v2_mat = rand(dim, 1);
 R_v1v1 = 0.25 * eye(dim);
 R_v2v2 = 0.75 * eye(dim);
 
-% Generate A1 and A2
+% Generate A1 and A2 % TODO: Cambiar esto a valores diferentes
 A1_mat = full(gallery('tridiag', dim, randn, randn, randn));
 A2_mat = full(gallery('tridiag', dim, randn, randn, randn));
 
@@ -63,16 +65,70 @@ R_x1x1 = A1_mat * R_ss * transpose(A1_mat) + R_v1v1;
 R_x2x2 = A2_mat * R_ss * transpose(A2_mat) + R_v2v2;
 R_x1x2 = A1_mat * R_ss * transpose(A2_mat);
 R_x2x1 = A2_mat * R_ss * transpose(A1_mat);
-R_sx1  = R_ss * transpose(A1_mat);
-R_sx2  = R_ss * transpose(A2_mat);
+R_sx1  = R_ss   * A1_mat';
+R_sx2  = R_ss   * A2_mat';
 R_x2s  = A2_mat * R_ss;
 
 % Calculate aditional covariance matrixes
-R_ee = R_x1x1 - R_x1x2 * transpose(C2_mat) * inv((C2_mat * R_x2x2 * transpose(C2_mat))) * C2_mat * R_x2x1;
-R_vv = R_ss - R_sx2 * transpose(C2_mat) * inv((C2_mat * R_x2x2 * transpose(C2_mat))) * C2_mat * R_x2s;
-R_ve = R_sx1 - R_sx2 * transpose(C2_mat) * inv((C2_mat * R_x2x2 * transpose(C2_mat))) * C2_mat * R_x2x1;
+% mat1 = C2_mat' * (C2_mat * R_x2x2 * C2_mat')^(-1) * C2_mat;
+% R_ee = R_x1x1 - R_x1x2 * mat1 * R_x2x1;
+% R_vv = R_ss   - R_sx2  * mat1 * R_x2s;
+% R_ve = R_sx1  - R_sx2  * mat1 * R_x2x1;
+% % R_ee = R_x1x1 - R_x1x2 * C2_mat' * (C2_mat * R_x2x2 * C2_mat')^(-1) * C2_mat * R_x2x1;
+% % R_vv = R_ss   - R_sx2  * C2_mat' * (C2_mat * R_x2x2 * C2_mat')^(-1) * C2_mat * R_x2s;
+% % R_ve = R_sx1  - R_sx2  * C2_mat' * (C2_mat * R_x2x2 * C2_mat')^(-1) * C2_mat * R_x2x1;
+
+% temp_mat = R_ve * R_ee^(-1) * R_ve';
+% [u_vec, lambda] = eig(temp_mat);
+
+% Calculate initial values of B1, B2, C1, C2
+% C1_mat = u_vec' * R_ve * R_ee^(-1);
+% C2_mat = u_vec' * R_ve * R_ee^(-1);
+% B1_mat = u_vec';
+% B2_mat = (R_sx2 - B1_mat * C1_mat * R_x1x2) * mat1;
 
 for k = 1 : max_iter
+
+    % Calculate aditional covariance matrixes
+    mat1 = C2_mat' * (C2_mat * R_x2x2 * C2_mat')^(-1) * C2_mat;
+    R_ee = R_x1x1 - R_x1x2 * mat1 * R_x2x1;
+    R_vv = R_ss   - R_sx2  * mat1 * R_x2s;
+    R_ve = R_sx1  - R_sx2  * mat1 * R_x2x1;
+
+    % Obtain eigenvalues matrix
+    temp_mat = R_ve * R_ee^(-1) * R_ve';
+    [u_vec, lambda] = eig(temp_mat);
+
+    % Calculate initial values of B1, B2, C1, C2
+    C1_mat_n = u_vec' * R_ve * R_ee^(-1);
+    C2_mat_n = u_vec' * R_ve * R_ee^(-1);
+    B1_mat_n = u_vec';
+    B2_mat_n = (R_sx2 - B1_mat * C1_mat * R_x1x2) * mat1;
+
+    % Calculate norms
+    norm_B1 = frob_norm((B1_mat_n - B1_mat));
+    norm_B2 = frob_norm((B2_mat_n - B2_mat));
+    norm_C1 = frob_norm((C1_mat_n - C1_mat));
+    norm_C2 = frob_norm((C2_mat_n - C2_mat));
+    % Calculate total error
+    tot_error = 0.25 * (norm_B1 + norm_B2 + norm_C1 + norm_C2);
+
+    error_vec = [error_vec, tot_error];
+
+    % Update matrixes
+    C1_mat = C1_mat_n;
+    C2_mat = C2_mat_n;
+    B1_mat = B1_mat_n;
+    B2_mat = B2_mat_n;
+
+    % Update covariance matrixes
+    R_x1x1 = A1_mat * R_ss * transpose(A1_mat) + R_v1v1;
+    R_x2x2 = A2_mat * R_ss * transpose(A2_mat) + R_v2v2;
+    R_x1x2 = A1_mat * R_ss * transpose(A2_mat);
+    R_x2x1 = A2_mat * R_ss * transpose(A1_mat);
+    R_sx1  = R_ss   * A1_mat';
+    R_sx2  = R_ss   * A2_mat';
+    R_x2s  = A2_mat * R_ss;
 
 
     if tot_error < tol
